d64d170dce8d0447c55fba0235ec6226b00c7b54
Valentin Lica <licavalentinh@gmail.com>
2022-12-24 21:37:50 +0100

    improvde footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { NextPage } from "next";
 
+import { CallIcon, PinIcon } from "@icon";
+
 import styles from "@styles/components/footer.module.scss";
 
 type Props = {
@@ -10,7 +12,12 @@ type Props = {
 const Footer: NextPage<Props> = ({ className = "" }) => {
   return (
     <footer className={`${styles.footer} ${className} container`}>
-      <p>5 Emerald Gardens, Dagenham RM8 1LH, United Kingdom</p>
+      <a href="https://maps.app.goo.gl/kD3JWZVBBCL2ZwkE8" target="_blank">
+        <PinIcon /> 5 Emerald Gardens, Dagenham RM8 1LH, United Kingdom
+      </a>
+      <a href="tel:+4407426961305">
+        <CallIcon /> +44 07426 961305
+      </a>
     </footer>
   );
 };
