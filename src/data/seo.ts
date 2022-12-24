78c9fb59c4c055898d628a16943f4e44fc5c47ce
Valentin Lica <licavalentinh@gmail.com>
2022-12-24 23:11:52 +0100

    working on ladnig

--- src/data/seo.ts
+++ src/data/seo.ts
@@ -1,6 +1,6 @@
 // Default Seo Data
 const seoData = {
-  title: "Locking Wheel Nut Removal UK",
+  title: "Locking Wheel Nut Removal",
   description:
     "Have you lost your locking wheel nut key?  Or is it broken or damaged? Has a mechanic already tried to remove it, maybe by drilling, and failed? Then we can help. Our fast and friendly mobile locking wheel nut removal service covers: *East London *Essex *Cambridgeshire *Kent  *south east *And many other locations  We can remove all four of your locking wheel nuts in around 10 to 15 minutes, and replace them with high quality standard wheel nuts or bolts if required. SUCCESSFUL REMOVAL GUARANTEED OR NO CHARGE. Call 07426 961305 and get yourself back on the road today!",
   image: "/logo.png",
